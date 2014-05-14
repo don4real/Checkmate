edd3aed483a36b7843a26637ca1eba9ebbd4412d
Julita Bielik <julz@o2.pl>
2014-05-14 15:06:46 +0200

    a

---
+++ src/ResClass/Match.java
@@ -0,0 +1,16 @@
+package ResClass;
+
+public class Match 
+{
+	Player player;
+	Player opponent;
+	
+	public Match(Player player, Player opponent) 
+	{
+		this.player = player;
+		this.opponent = opponent;
+	}
+	
+	
+		
+}
